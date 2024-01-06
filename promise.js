14725b12f53a3e37f8c5f9393ea1900e89f6e95e
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-01-06 21:50:53 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,3 +1,18 @@
+/*This code shows the synchronous execution of asynchronous promises. Each task/promise gets execute in a sequential manner.
+P1) Wake Up
+P2) Drink Water
+P3) Brush teeth
+P4) Take a Bath
+P5) Worship 
+
+|¯¯¯¯¯¯¯¯¯¯¯|------->|¯¯¯¯¯¯¯¯¯¯¯|------->|¯¯¯¯¯¯¯¯¯¯¯|------->|¯¯¯¯¯¯¯¯¯¯¯|------->|¯¯¯¯¯¯¯¯¯¯¯|------->|¯¯¯¯¯¯¯¯¯¯¯|
+|    P1     |------->| P1.then() |------->| P2.then() |------->| P3.then() |------->| P4.then() |------->| P5.then() |  
+|           |------->|           |------->|           |------->|           |------->|           |------->|           |
+|           |------->|    P2     |------->|    P3     |------->|    P4     |------->|    P5     |------->|           |
+|___________|------->|___________|------->|___________|------->|___________|------->|___________|------->|___________|
+
+*/
+
 var p1 = new Promise((res, rej)=>{
     return res("Wake Up");
 })
